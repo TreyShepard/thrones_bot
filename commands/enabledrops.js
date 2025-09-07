80bc80a33a21ae038c1d45af5863128da46d6e98
cwoster <email@email.com>
2025-09-07 11:20:41 -0500

    Refactor to interaction.

--- commands/enabledrops.js
+++ commands/enabledrops.js
@@ -3,7 +3,7 @@
 module.exports = {
   name: "enabledrops",
   description: "Enables drops for the clan.",
-  execute(message) {
-    message.channel.send("Drops have been enabled for the clan! Enjoy your rewards!");
+  async execute(interaction) {
+    await interaction.reply("Drops have been enabled for the clan! Enjoy your rewards!");
   },
 };
