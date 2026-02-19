da933a2c3d3267e59aa7974b9cfab20072f16d65
cwoster <email@email.com>
2026-02-19 14:34:10 -0600

    Update setup command.

--- commands/setup.js
+++ commands/setup.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const COLLECTION_WEBHOOK = "https://discord.com/api/webhooks/1282421845198373095/KoV3fyDEU1QK-LdtulyNVqd6yNc5wBEW7OlvtZjj_g5T4PHifND1T1LkDC9QXcY2MZEV";
+const COLLECTION_WEBHOOK = "https://discord.com/api/webhooks/1463596277265731584/XbaDJtH9_pbfbDFbxHE5Rf72PWeoCQL-vjYOICKt8F-QEOe2RQI-iiSpSZSDioc0f6im";
+const LOOT_WEBHOOK = "https://discord.com/api/webhooks/1463596005663440896/Zp17AAE-goehXnnaBRpsyu9vNvHsOr3Jv_MjmHFBigFMsAM5ZeMIm2wSp7YMurLRgIlC";
 
 module.exports = {
   name: "setup",
@@ -17,9 +18,9 @@ module.exports = {
           "Hey there! We use the following plugins:\n" +
           "1. Discord Collection Logger (see <#1282421548074012784>)\n" +
           `   - Webhook: ${COLLECTION_WEBHOOK}\n` +
-          "2. Discord Loot Logger (see <#1282421548074012784>)\n" +
+          "2. Discord Loot Logger (see <#1463595426358755472>)\n" +
           `   - Loot Value = 3,000,000\n` +
-          `   - Webhook: ${COLLECTION_WEBHOOK}\n` +
+          `   - Webhook: ${LOOT_WEBHOOK}\n` +
           "3. XP Updater (used for Competition tracking)\n\n" +
           "Please see the attached screenshots for the advised plugin setups.",
         files: [
